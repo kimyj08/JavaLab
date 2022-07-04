430140b9aa83865d7967d2b15f5939fd1e441a4a
Julia <108450096+kimyj08@users.noreply.github.com>
2022-07-04 17:32:33 +0900

    7/4

---
+++ day3_6.java
@@ -0,0 +1,10 @@
+public class day3_6 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++) {             // i = 1 2 3
+            for(int j=1;j<=i;j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
