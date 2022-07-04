3decc3766ccd632cb021f3bcc0894d07ec9e0504
Julia <108450096+kimyj08@users.noreply.github.com>
2022-07-04 18:03:47 +0900

    7/4

--- day3_2.java
+++ day3_2.java
@@ -1,6 +1,6 @@
 public class day3_2 {
     public static void main(String[] args) {
-        System.out.println("i \t 2*1 \t 2*i-1 \t i*1 \t 11-i \t i%3 \t i/3");
+        System.out.println("i \t 2*1 \t 2*i-1 \t i*i \t 11-i \t i%3 \t i/3");
         System.out.println("----------------------------------------------------");
 
         for(int i=1;i<=10;i++)
