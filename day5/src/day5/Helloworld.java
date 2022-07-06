15224839351b92544ff69ef65c1cd910f6477596
Julia <108450096+kimyj08@users.noreply.github.com>
2022-07-06 10:15:16 +0900

    7/6

---
+++ day5/src/day5/Helloworld.java
@@ -0,0 +1,10 @@
+package day5;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World ~~~사랑해요 연예가 중계");
+	}
+
+}
