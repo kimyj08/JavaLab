0b9327b5eb3bb13ed7d2f8bb400e723338867f38
Julia <108450096+kimyj08@users.noreply.github.com>
2022-07-07 18:03:55 +0900

    Update TvTest.java

--- day6/src/day6_1/TvTest.java
+++ day6/src/day6_1/TvTest.java
@@ -7,7 +7,7 @@ public class TvTest {
 		// t1 객체 생성, Tv클래스로 t1 인스턴스 생성
 		
 		Tv t1 = new Tv();  // t1 객체 생성
-		Tv t2 = new Tv();  // t1 객체 생성
+		Tv t2 = new Tv();  // t2 객체 생성
 		
 		t1.channel = 7;  // channel 번호 7번으로 초기화
 		t1.channelDown();  // channel 변경 하나 내림 7 -> 6
