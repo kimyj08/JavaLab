8c4b2b236ceee05eda33ea838ed9a6d350a1ca1f
Julia <108450096+kimyj08@users.noreply.github.com>
2022-07-14 18:11:28 +0900

    7/14

---
+++ day11/src/day11_1/Sample.java
@@ -0,0 +1,15 @@
+package day11_1;
+
+import java.util.ArrayList;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		ArrayList<String> pitches = new ArrayList<>();
+		pitches.add("138");
+		pitches.add("129");
+		pitches.add("142");
+		System.out.println(pitches);
+	}
+
+}
---
+++ day11/src/day11_2/Sample.java
@@ -0,0 +1,13 @@
+package day11_2;
+
+import java.util.HashMap;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		HashMap<String, String> map = new HashMap<>();
+		map.put("people", "사람");
+		map.put("baseball", "야구");
+		System.out.println(map.get("people"));
+	}
+}
